8c1680eebce38f8eb274c62844771d242105bd2e
known-person <known-person@jinmobility.com>
2023-05-01 23:52:05 +0900

    first commit

---
+++ src/main/java/com/jinnyjinnyjinjin/projecteseok/ProjectESeokApplication.java
@@ -0,0 +1,13 @@
+package com.jinnyjinnyjinjin.projecteseok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectESeokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectESeokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinnyjinnyjinjin/projecteseok/ProjectESeokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinnyjinnyjinjin.projecteseok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectESeokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
