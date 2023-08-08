8a686f5b1fc61d787d162a6a4e8e22bcd483e46d
jinnyjinnyjinjin <kkam2ya@gmail.com>
2023-08-09 00:06:40 +0900

    템플릿 설정 변경

--- src/main/java/com/jinnyjinnyjinjin/projecteseok/MainController.java
+++ src/main/java/com/jinnyjinnyjinjin/projecteseok/MainController.java
@@ -1,12 +1,12 @@
 package com.jinnyjinnyjinjin.projecteseok;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/index")
+    @GetMapping("/")
     public String index() {
         return "index";
     }
