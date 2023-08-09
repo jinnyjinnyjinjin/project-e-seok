97555bbe0edb4432fbbb7c3b2b0645aeeb1b4d21
jinnyjinnyjinjin <kkam2ya@gmail.com>
2023-08-09 17:52:16 +0900

    Controller path 변경

--- src/main/java/com/jinnyjinnyjinjin/projecteseok/contorller/MainController.java
+++ src/main/java/com/jinnyjinnyjinjin/projecteseok/contorller/MainController.java
@@ -1,12 +1,14 @@
 package com.jinnyjinnyjinjin.projecteseok.contorller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping
 public class MainController {
 
-    @RequestMapping
+    @GetMapping("/")
     public String index() {
         return "index";
     }
